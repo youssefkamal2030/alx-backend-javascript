f2be6ea643408d4e59e7f82f426cd94f213921d1
youssef <youssefkamal856@yahoo.com>
2024-09-30 18:31:23 +0300

    done

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++
@@ -1,7 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-  for (const item of reportWithIterator) {
-    employees.push(item);
-  }
-  return employees.join(' | ');
-}
