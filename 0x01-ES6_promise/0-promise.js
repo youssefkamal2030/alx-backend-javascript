b57852b522c1656aaec798180d554bd9f16811f5
youssef <youssefkamal856@yahoo.com>
2024-10-02 18:23:21 +0300

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
 export default function getResponseFromAPI() {
-  const promise = new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
-      resolve(true);
-    } else {
-      reject(new Error('false'));
-    }
-  });
-  return promise;
+  return new Promise(() => {});
 }
