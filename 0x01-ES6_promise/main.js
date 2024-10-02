dc87b6f6b354daa2005592d8cda32d37154784e3
youssef <youssefkamal856@yahoo.com>
2024-10-02 19:05:42 +0300

    done

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,4 +1,5 @@
-import getResponseFromAPI from "./0-promise.js";
+import getFullResponseFromAPI from './1-promise';
 
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
+
