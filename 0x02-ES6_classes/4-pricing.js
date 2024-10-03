4f16997857e9e57e5adceb250ba0d0671b7e2dad
youssef <youssefkamal856@yahoo.com>
2024-10-03 19:17:29 +0300

    done

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,33 +1,32 @@
-import Currency from "./3-currency";
-export default class Pricing
-{
-    constructor(amount,Currency)
-    {
-        this._amount=amount;
-        this._Currency=Currency;
-    }
-    get amount()
-    {
-        return this._amount
-    }
-    set amount(amount)
-    {
-        this._amount=amount;
-    }
-    get Currency()
-    {
-        return this._Currency;
-    }
-    set Currency(Currency)
-    {
-        this._Currency=Currency;
-    }
-    displayFullPrice()
-    {
-        return `${this._amount} ${Currency.displayFullPrice}`
-    }
-    static convertPrice(amount,conversionRate)
-    {
-        return amount * conversionRate
-    }
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, Currency) {
+    this._amount = amount;
+    this._Currency = Currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get Currency() {
+    return this._Currency;
+  }
+
+  set Currency(Currency) {
+    this._Currency = Currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${Currency.displayFullPrice}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
