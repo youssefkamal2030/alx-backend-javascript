616e18a41d99352936673ab4d4fdaed8f97aced3
youssef <youssefkamal856@yahoo.com>
2024-10-03 19:57:28 +0300

    done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,26 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class Building directly');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  /**
+     * @param {Number} sqft
+     */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
