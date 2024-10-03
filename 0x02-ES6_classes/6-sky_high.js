4719f59d4cd62c13b72fb4c1e60a6f2d8bbe500f
youssef <youssefkamal856@yahoo.com>
2024-10-03 20:09:51 +0300

    done

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,25 +1,37 @@
-import Building from "./5-building";
-export default class SkyHighBuilding extends Building
-{
-    constructor(sqft,floors)
-    {
-        super(sqft)
-        this._floor=floors;
-    }
-    get floors()
-    {
-        return this._floor;
-    }
-    get sqft()
-    {
-        return this._sqft
-    }
-    set sqft(sqft)
-    {
-        this._sqft=sqft;
-    }
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this.floors} floors`;
-      }
+import Building from './5-building';
 
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof sqft !== 'number' || typeof floors !== 'number') {
+      throw new TypeError('Provide valid dimensions and number of floors');
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set sqft(feet) {
+    if (typeof feet !== 'number') {
+      throw new TypeError('Provide Valid Dimensions');
+    }
+    this._sqft = feet;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw new TypeError('Provide valid number of floors');
+    }
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
