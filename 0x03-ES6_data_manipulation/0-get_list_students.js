6acacbe8a611629f10b88548d7da94d547ec17d3
youssef <youssefkamal856@yahoo.com>
2024-10-07 20:18:43 +0300

    done

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents() {
+  const o1 = {
+    id: 1,
+    firstname: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const o2 = {
+    id: 2,
+    firstname: 'James',
+    location: 'Columbia',
+  };
+  const o3 = {
+    id: 5,
+    firstname: 'Serena',
+    location: 'San Francisco',
+  };
+  return [o1, o2, o3];
+}
