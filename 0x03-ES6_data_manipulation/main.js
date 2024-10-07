f161fb127e50b32e2005d5e79cbade51d2ff36e0
youssef <youssefkamal856@yahoo.com>
2024-10-07 20:35:57 +0300

    done

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,5 +1,5 @@
-import getListStudentIds from "./1-get_list_student_ids.js";
 import getListStudents from "./0-get_list_students.js";
+import getStudentsByLocation from "./2-get_students_by_loc.js";
 
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
+const students = getListStudents();
+console.log(getStudentsByLocation(students, 'San Francisco'));
