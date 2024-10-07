2dc029afffcc709cabc53e83c739061c159c8520
youssef <youssefkamal856@yahoo.com>
2024-10-07 20:29:04 +0300

    done

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,4 @@
-export default function getListStudentsIds(arr)
-{
-    if(!Array.isArray())
-        return [];
-    
+export default function getListStudentsIds(arr) {
+  if (!(arr instanceof Array)) return [];
+  return arr.map((item) => item.id);
 }
