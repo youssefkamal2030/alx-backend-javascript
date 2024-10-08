1d367c657353e4808310bbc8ffecfbe497e8b9b0
youssef <youssefkamal856@yahoo.com>
2024-10-08 17:28:04 +0300

    done

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,6 +1,3 @@
-import getListStudents from "./0-get_list_students.js";
-import updateStudentGradeByCity from "./4-update_grade_by_city.js";
+import createInt8TypedArray from "./5-typed_arrays.js";
 
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
-
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
+console.log(createInt8TypedArray(10, 2, 89));
